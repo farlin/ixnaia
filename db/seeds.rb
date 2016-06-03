16b798630191e1d00de926f2e27d580ce3e2c50f
Nishat Ara Alam <farlin@gmail.com>
2016-06-04 01:35:03 +1000

    cleaning up seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,36 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-#
 
+# Added some students..
+# making sure the seed data always retain the same set of Ids
+Student.where(id: 1..5).destroy_all
 
 students = Student.create([
-    { 
-        name: 'Gi' 
-    }, 
-    { 
-        name: 'Kwame' 
-    }, 
-    { 
-        name: 'Linka' 
-    }, 
-    { 
-        name: 'Ma-Ti' 
-    }, 
-    { 
-        name: 'Wheeler' 
-    }
+    { id: 1, name: 'Gi'       }, 
+    { id: 2, name: 'Kwame'    }, 
+    { id: 3, name: 'Linka'    }, 
+    { id: 4, name: 'Ma-Ti'    }, 
+    { id: 5, name: 'Wheeler'  }
 ])
 
-# we are supposed to have 100 lessons
 #
+# We are supposed to have 100 lessons
+#
+Lesson.where(id: 1..100).destroy_all
+LessonPart.where(id: 1..300).destroy_all
+
+xdx = 1
 100.times do |i|
-  i_ref = i+1   # because the loop starts at zero
-  lesson = Lesson.create(title: "Lesson ##{i_ref}", content: "In this particular lesson #{i_ref}, student will learn all about..")
+  idx = i+1   # making sure we start our numbers at 1
+  lesson = Lesson.create(id: idx, title: "Lesson ##{idx}", content: "In this particular lesson #{idx}, student will learn all about..")
   
   # each lesson will have 3 parts
   #
   3.times do |x|
-      x_ref = x + 1
-      LessonPart.create(name: "Lesson ##{i_ref} Part ##{x_ref}", lesson: lesson, sequence: x_ref)
+      seq = x + 1
+      LessonPart.create(id: xdx, name: "Lesson ##{idx} Part ##{seq}", lesson: lesson, sequence: seq)
+      
+      xdx = xdx + 1 # incrementing the index for lesson parts
   end
 end
