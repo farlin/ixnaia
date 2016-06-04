a3bfc5f3b2503da11a67d20b5d45d1f69254855b
Nishat Ara Alam <farlin@gmail.com>
2016-06-04 18:25:35 +1000

    association for students with lesson parts

--- app/models/lesson_part.rb
+++ app/models/lesson_part.rb
@@ -1,3 +1,4 @@
 class LessonPart < ActiveRecord::Base
   belongs_to :lesson, inverse_of: :lesson_parts
+  has_many :lesson_trackers, inverse_of: :lesson_parts
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,4 @@
 class Student < ActiveRecord::Base
     has_many :lesson_trackers, inverse_of: :student
+    has_many :lesson_parts, through: :lesson_trackers
 end
