669a6a0dd057ddfdca46d4d18503f7407db88bbc
Nishat Ara Alam <farlin@gmail.com>
2016-06-04 18:23:54 +1000

    returning the root item with json response

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module Ixnaia
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    ActiveRecord::Base.include_root_in_json = true
   end
 end
