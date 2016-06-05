19362c3cd182532076d35d60613c9ddec77383ee
Nishat Ara Alam <farlin@gmail.com>
2016-06-05 13:35:49 +1000

    added failed test for lesson model

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -2,6 +2,12 @@
 require 'rails_helper'
 
 RSpec.describe Lesson, type: :model do
-    it "is invalid without a title"
-    it "has 3 lesson parts"
+    
+    it "is invalid without a title" do
+        lesson = Lesson.new(title: nil)
+        lesson.valid?
+        expect(lesson.errors[:title]).to include("can't be blank")
+    end
+    
+    # it "has 3 lesson parts"
 end
