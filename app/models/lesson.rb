23eeb92433615b0003f60ba2a960a94d72a25e93
Nishat Ara Alam <farlin@gmail.com>
2016-06-05 17:01:01 +1000

    added validation for association

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,7 +1,13 @@
 class Lesson < ActiveRecord::Base
+    
+    # === relationships ====
     has_many :lesson_parts, inverse_of: :lesson
     
     # === validations ====
     validates :title, :presence => true
+    validate :validate_lesson_parts 
   
+    def validate_lesson_parts
+      errors.add(:lesson_parts, "too many parts") if lesson_parts.size > 3
+    end
 end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -9,5 +9,16 @@ RSpec.describe Lesson, type: :model do
         expect(lesson.errors[:title]).to include("can't be blank")
     end
     
-    # it "has 3 lesson parts"
+    
+    it "is invalid when there are more than 3 parts" do
+        lesson = Lesson.new(title: 'Lesson 1')
+        
+        4.times do
+            LessonPart.create! lesson:lesson, name: "part", sequence: 0
+        end
+        
+        lesson.valid?
+        expect(lesson.errors[:lesson_parts]).to include("too many parts")
+    end
+
 end
